c9332149eced792bdeed0b4f03b588180d6aee44
Sofiia Horokhova <sofiakisel5@gmail.com>
2026-04-28 00:21:54 +0200

    fix: flake8 style issues

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -11,7 +11,7 @@ class CarWashStation:
                  clean_power: int,
                  average_rating: float,
                  count_of_ratings: int,
-                 ):
+                 ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -20,10 +20,10 @@ class CarWashStation:
     def serve_cars(self, cars: list) -> float:
         income = 0
         for car in cars:
-           if self.clean_power > car.clean_mark:
-               price = self.calculate_washing_price(car)
-               income += price
-               self.wash_single_car(car)
+            if self.clean_power > car.clean_mark:
+                price = self.calculate_washing_price(car)
+                income += price
+                self.wash_single_car(car)
         return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
@@ -36,10 +36,9 @@ class CarWashStation:
 
     def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
-           car.clean_mark = self.clean_power
+            car.clean_mark = self.clean_power
 
     def rate_service(self, score: int) -> None:
         total_score = (self.average_rating * self.count_of_ratings) + score
         self.count_of_ratings += 1
         self.average_rating = round(total_score / self.count_of_ratings, 1)
-
